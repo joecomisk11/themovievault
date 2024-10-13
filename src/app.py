71cf20c5a199a1274918efbcf9874563e0fe19cc
Joe Comiskey <joecomiskey_asset@outlook.com>
2024-10-12 20:28:20 -0400

    fix import

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager, login_user, login_required, \
     logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from src.models import db, User, Favorite
+from models import db, User, Favorite
 
 app = Flask(__name__)
 
